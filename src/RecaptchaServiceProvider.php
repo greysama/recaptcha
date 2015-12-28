ba44f522d1c567eaff5b0d9aa968173db4b4a8d2
greysama <greysama86@gmail.com>
2015-12-28 15:03:47 -0300

    Update RecaptchaServiceProvider.php

--- src/RecaptchaServiceProvider.php
+++ src/RecaptchaServiceProvider.php
@@ -40,7 +40,7 @@ class RecaptchaServiceProvider extends ServiceProvider
             $challenge = app('Input')->get($captcha->getResponseKey());
 
             return $captcha->check($challenge, $value);
-        }, 'Please ensure that you are a human!');
+        }, 'Por favor, confirma que eres humano!');
     }
 
     /**
